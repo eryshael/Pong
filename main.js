c0c1abf4549fe57e2275d66e8d119ced79958fca
Eryshael <morgan.layne.blake@gmail.com>
2026-09-10 23:54:39 -0400

    Formatted files

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ context.font = "32px Arial";
 const p1W = 12;
 const p1H = 64;
 let p1X = 16;
-let p1Y = (height / 2) - (p1H / 2);
+let p1Y = height / 2 - p1H / 2;
 let p1Score = 0;
 
 context.fillRect(p1X, p1Y, p1W, p1H);
@@ -23,18 +23,18 @@ context.fillText(p1Score, width / 4 - 12, 40);
 const p2W = p1W;
 const p2H = p1H;
 let p2X = width - p2W - 16;
-let p2Y = (height / 2) - (p2H / 2);
+let p2Y = height / 2 - p2H / 2;
 let p2Score = 0;
-context.fillText(p2Score, width / 4 * 3 - 12, 40);
+context.fillText(p2Score, (width / 4) * 3 - 12, 40);
 
 context.fillRect(p2X, p2Y, p2W, p2H);
 
 // Ball
 const ballW = 16;
 const ballH = 16;
-let ballOriginX = (width / 2) - (ballW / 2);
-let ballOriginY = (height / 2) - (ballH / 2);
-let ballPrevX = ballX = ballOriginX;
+let ballOriginX = width / 2 - ballW / 2;
+let ballOriginY = height / 2 - ballH / 2;
+let ballPrevX = (ballX = ballOriginX);
 let ballY = ballOriginY;
 let ballDX = 8;
 let ballDY = 4;
@@ -48,42 +48,46 @@ document.addEventListener("keyup", (e) => keysDown.delete(e.key));
 
 function update() {
   // Ball collision against wall
-  if(ballX + ballW >= width) {
+  if (ballX + ballW >= width) {
     ballDX = -ballDX;
     ballX = ballOriginX;
     ballY = ballOriginY;
     p1Score++;
-    if(p1Score == 11) game = false;
+    if (p1Score == 11) game = false;
   }
-  
-  if(ballX <= 0) {
+
+  if (ballX <= 0) {
     ballDX = -ballDX;
     ballX = ballOriginX;
     ballY = ballOriginY;
     p2Score++;
-    if(p2Score == 11) game = false;
+    if (p2Score == 11) game = false;
   }
-  
-  if(ballY + ballH >= height) {ballDY = -ballDY}
-  if(ballY <= 0) {ballDY = -ballDY}
+
+  if (ballY + ballH >= height) {
+    ballDY = -ballDY;
+  }
+  if (ballY <= 0) {
+    ballDY = -ballDY;
+  }
   // ballPrevX = ballX
   // ballX += ballDX;
   // ballY += ballDY;
 
   // Ball collision against P1
-  if(ballX <= p1X + p1W && ballY >= p1Y && ballY <= p1Y + p1H) {
-    ballDX = -ballDX
+  if (ballX <= p1X + p1W && ballY >= p1Y && ballY <= p1Y + p1H) {
+    ballDX = -ballDX;
   }
 
   // Ball collision against P2
-  if(ballX + ballW >= p2X && ballY >= p2Y && ballY <= p2Y + p2H) {
-    ballDX = -ballDX
+  if (ballX + ballW >= p2X && ballY >= p2Y && ballY <= p2Y + p2H) {
+    ballDX = -ballDX;
   }
-  
-  if(keysDown.has("w") && p1Y >= 0) p1Y -= 4;
-  if(keysDown.has("s") && p1Y + p1H <= height) p1Y += 4;
-  if(keysDown.has("ArrowUp") && p2Y >= 0) p2Y -= 4;
-  if(keysDown.has("ArrowDown") && p2Y + p2H <= height) p2Y += 4;
+
+  if (keysDown.has("w") && p1Y >= 0) p1Y -= 4;
+  if (keysDown.has("s") && p1Y + p1H <= height) p1Y += 4;
+  if (keysDown.has("ArrowUp") && p2Y >= 0) p2Y -= 4;
+  if (keysDown.has("ArrowDown") && p2Y + p2H <= height) p2Y += 4;
 }
 
 function draw() {
@@ -92,12 +96,12 @@ function draw() {
   context.fillRect(p2X, p2Y, p2W, p2H); // Draw P2
   context.fillRect(ballX, ballY, ballW, ballH); // Draw Ball
   context.fillText(p1Score, width / 4 - 12, 40);
-  context.fillText(p2Score, width / 4 * 3 - 12, 40);
+  context.fillText(p2Score, (width / 4) * 3 - 12, 40);
 }
 
 setInterval(() => {
-  if(game) {
+  if (game) {
     update();
-    draw()
+    draw();
   }
-}, 1000/60);
+}, 1000 / 60);
